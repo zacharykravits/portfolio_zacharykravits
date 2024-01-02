62295f3a283193ab182867668bdb888f3e4e74ba
Zach <zacharykravits@protonmail.com>
2024-01-02 18:33:40 -0500

    add new pages

--- app.js
+++ app.js
@@ -18,9 +18,9 @@ const index = async () => {
     });
 }
 
-const about = async () => {
+const work_uxui_chugsrus = async () => {
     const decoder = new TextDecoder("utf-8")
-    const file = await Deno.readFile(`${Deno.cwd()}/pages/about.html`)
+    const file = await Deno.readFile(`${Deno.cwd()}/pages/uxui-chugsrus.html`)
     const data = decoder.decode(file)
     return new Response(data, {
         status: 200,
@@ -30,16 +30,88 @@ const about = async () => {
     });
 }
 
-const contact = async () => {
+const work_uxui_renewproject = async () => {
     const decoder = new TextDecoder("utf-8")
-    const file = await Deno.readFile(`${Deno.cwd()}/pages/contact.html`)
+    const file = await Deno.readFile(`${Deno.cwd()}/pages/uxui-renewproject.html`)
     const data = decoder.decode(file)
     return new Response(data, {
         status: 200,
         headers: {
-            "content-type": "text/html"
+          "content-type": "text/html",
         }
-    })
+    });
+}
+
+const work_uxui_dkprojectbuilding = async () => {
+    const decoder = new TextDecoder("utf-8")
+    const file = await Deno.readFile(`${Deno.cwd()}/pages/uxui-dkprojectbuilding.html`)
+    const data = decoder.decode(file)
+    return new Response(data, {
+        status: 200,
+        headers: {
+          "content-type": "text/html",
+        }
+    });
+}
+
+const work_uxui_elburro = async () => {
+    const decoder = new TextDecoder("utf-8")
+    const file = await Deno.readFile(`${Deno.cwd()}/pages/uxui-elburro.html`)
+    const data = decoder.decode(file)
+    return new Response(data, {
+        status: 200,
+        headers: {
+          "content-type": "text/html",
+        }
+    });
+}
+
+const work_other_idelic = async () => {
+    const decoder = new TextDecoder("utf-8")
+    const file = await Deno.readFile(`${Deno.cwd()}/pages/other-idelic.html`)
+    const data = decoder.decode(file)
+    return new Response(data, {
+        status: 200,
+        headers: {
+          "content-type": "text/html",
+        }
+    });
+}
+
+const work_other_dkprojectbuilding = async () => {
+    const decoder = new TextDecoder("utf-8")
+    const file = await Deno.readFile(`${Deno.cwd()}/pages/other-dkprojectbuilding.html`)
+    const data = decoder.decode(file)
+    return new Response(data, {
+        status: 200,
+        headers: {
+          "content-type": "text/html",
+        }
+    });
+}
+
+const work_other_authvow = async () => {
+    const decoder = new TextDecoder("utf-8")
+    const file = await Deno.readFile(`${Deno.cwd()}/pages/other-authvow.html`)
+    const data = decoder.decode(file)
+    return new Response(data, {
+        status: 200,
+        headers: {
+          "content-type": "text/html",
+        }
+    });
+}
+
+const work_other_authvowcsslibrary = async () => {
+    const decoder = new TextDecoder("utf-8")
+    const file = await Deno.readFile(`${Deno.cwd()}/pages/other-authvowcsslibrary.html`)
+    const data = decoder.decode(file)
+    return new Response(data, {
+        status: 200,
+        headers: {
+          "content-type": "text/html",
+        }
+    });
 }
 
 const notFound = async () => {
@@ -66,10 +138,22 @@ const reqHandler = async (req) => {
     switch (pathname) {
         case "/":
             return index();
-        case "/about":
-            return about();
-        case "/contact":
-            return contact();
+        case "/uxui/chugsrus":
+            return work_uxui_chugsrus();
+        case "/uxui/renewproject":
+            return work_uxui_renewproject();
+        case "/uxui/dkprojectbuilding":
+            return work_uxui_dkprojectbuilding();
+        case "/uxui/elburro":
+            return work_uxui_authvow();
+        case "/other/idelic":
+            return work_other_idelic();
+        case "/other/dkprojectbuilding":
+            return work_other_dkprojectbuilding();
+        case "/other/authvow":
+            return work_other_authvow();
+        case "/other/authvowcsslibrary":
+            return work_other_authvowcsslibrary();
         default:
             return notFound();
             break;
