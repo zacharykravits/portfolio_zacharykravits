c11fd85f18dbfaed7f88a0f8d5c7d470994eb932
Zach <zacharykravits@protonmail.com>
2024-02-16 09:26:15 -0500

    update

--- utils/getallprojectsjson-utils.js
+++ utils/getallprojectsjson-utils.js
@@ -5,12 +5,14 @@ export const getAllProjectsJSON = async (desiredProjectType) => {
 
     for await (const project of desiredDir) {
         const filePath = `${Deno.cwd()}/models/${desiredProjectType}/${project.name}`
+        // const fileToImport = `${Deno.cwd()}/models/${desiredResource}/public/${file.name}`
         console.log('filePath in desiredDir loop: ', filePath)
         const JSONmodel = await import(filePath, {
             with: {
                 type: "json"
             }
         });
+        console.log("JSONmodel.default.basics: ", JSONmodel.default.basics)
         projects.push(JSONmodel.default.basics)
     }
     return projects
